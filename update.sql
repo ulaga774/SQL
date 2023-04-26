12bbb50b61dddbe90c533ab502a7ecc50c1e53f1
ulaga774 <ulaga774xworkz@gmail.com>
2023-04-26 20:45:30 +0530

    update and insert

---
+++ update.sql
@@ -0,0 +1,195 @@
+CREATE DATABASE Movie;
+CREATE TABLE Movie_Info(I_id int ,MovieName varchar(30) ,budget int , director varchar(30) , hero varchar(30) , 
+villan varchar(30),boxOffice int, cast varchar(30) , heronie varchar(30) , assistentDirector varchar(30),
+producer varchar(30),supportingActor varchar(30) ,supportingActors varchar(30) ,heronies varchar(30) ,
+heroSalary bigint ,heronieSalary bigint , directorSalary bigint ,villanSalary bigint ,supportingActorSalary int,
+AgentName varchar(30));
+
+SELECT *FROM Movie_Info;
+INSERT INTO Movie_Info VALUES (1,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (2,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (3,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (4,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (5,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (6,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (7,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (8,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (9,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (10,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (11,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (12,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (13,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (14,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (15,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (16,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (17,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (18,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (19,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (20,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (21,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (22,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (23,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (24,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (26,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (27,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (28,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (29,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (30,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (31,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (32,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (33,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (34,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (35,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (36,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (37,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (38,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (39,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+INSERT INTO Movie_Info VALUES (40,'vikram',1,'lokesh','kamal','vs',10,'amar','amala','ulaga',
+'kiran','varun','shubes','sandhya',100,1000,10000,100000,10000000,'tina');
+use movie;
+
+//MovieName
+UPDATE Movie_Info set MovieName = 'make it' where I_id = 2;
+//budget
+UPDATE Movie_Info set budget = 12345678 where I_id = 2;
+//director
+UPDATE Movie_Info set director = 'ulaga' where I_id = 2;
+//hero
+UPDATE Movie_Info set hero = 'varun' where I_id = 2;
+//villan
+UPDATE Movie_Info set villan = 'sanjay' where I_id = 2;
+//boxOffice
+UPDATE Movie_Info set boxOffice = 87654321 where I_id = 2;
+//cast 
+UPDATE Movie_Info set cast = 'mass' where I_id = 2;
+//heronie
+UPDATE Movie_Info set heronie = 'samantha' where I_id = 2;
+//assistentDirector
+UPDATE Movie_Info set assistentDirector = 'suresh' where I_id = 2;
+//producer
+UPDATE Movie_Info set producer = 'Siva Kumar' where I_id = 2;
+//supportingActor
+UPDATE Movie_Info set supportingActor = 'hotstar' where I_id = 2;
+//supportingActors
+UPDATE Movie_Info set supportingActors = 'kiran' where I_id = 2;
+//heronies
+UPDATE Movie_Info set heronies = 'idiot' where I_id = 2;
+//heroSalary
+UPDATE Movie_Info set heroSalary = 10000000 where I_id = 2;
+//heronieSalary
+UPDATE Movie_Info set heronieSalary = 1000000 where I_id = 2;
+//directorSalary
+UPDATE Movie_Info set directorSalary = 000 where I_id = 2;
+//villanSalary
+UPDATE Movie_Info set villanSalary = 100000000 where I_id = 2;
+//supportingActorSalary
+UPDATE Movie_Info set supportingActorSalary = 10 where I_id = 2;
+//AgentName
+UPDATE Movie_Info set AgentName = 'ulagaHotstar' where I_id = 2;
+
+CREATE TABLE movies_count(lokesh int ,ulaga int , varun int ,kiran int , mass int ,
+manirathanam int, vetriMaran int,attlee int ,vinoth int ,siva int ,vigneshSivan int ,nelson int,
+mari int ,suresh int ,tamil int ,barani int ,verra int ,pugal int , sukumar int ,sandy int);
+
+SELECT * FROM movies_count;
+
+INSERT INTO movies_count VALUES (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (2,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (3,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (4,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (5,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (6,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (7,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (8,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (9,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (10,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (11,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (12,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (13,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (14,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (15,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (16,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (17,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (18,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (19,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (20,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (21,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (22,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (23,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (24,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (25,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (26,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (27,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (28,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (29,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (30,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (31,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (32,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (33,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (34,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (35,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (36,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (37,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (38,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (39,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+INSERT INTO movies_count VALUES (40,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+USE movie;
+UPDATE movies_count set ulaga = 12 where lokesh = 2;
+UPDATE movies_count set varun = 121 where lokesh = 2;
+UPDATE movies_count set kiran = 122 where lokesh = 2;
+UPDATE movies_count set mass = 123 where lokesh = 2;
+UPDATE movies_count set manirathanam = 124 where lokesh = 2;
+UPDATE movies_count set vetriMaran = 125 where lokesh = 2;
+UPDATE movies_count set attlee = 126 where lokesh = 2;
+UPDATE movies_count set vinoth = 127 where lokesh = 2;
+UPDATE movies_count set siva = 128 where lokesh = 2;
+UPDATE movies_count set vigneshSivan = 129 where lokesh = 2;
+UPDATE movies_count set nelson = 120 where lokesh = 2;
+UPDATE movies_count set mari = 1222 where lokesh = 2;
+UPDATE movies_count set suresh = 1213 where lokesh = 2;
+UPDATE movies_count set tamil = 172 where lokesh = 2;
+UPDATE movies_count set barani = 1762 where lokesh = 2;
+UPDATE movies_count set verra = 142 where lokesh = 2;
+UPDATE movies_count set pugal = 152 where lokesh = 2;
+UPDATE movies_count set sukumar = 112 where lokesh = 2;
+UPDATE movies_count set sandy = 102 where lokesh = 2;
+
